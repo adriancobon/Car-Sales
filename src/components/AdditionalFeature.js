6550d3e9cc2c055df6ed24f48e7f5c62287fda23
adriancobon <adriancobon@outlook.com>
2019-09-11 14:44:37 -0400

    Completed Project

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const AdditionalFeature = props => {
+
+  const addFeature = () => {
+    props.addNewFeature(props.feature)
+  }
+
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={addFeature} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+
+
 
 export default AdditionalFeature;
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
+import { connect } from 'react-redux';
+
+import { addNewFeature } from '../actions/carActions';
+
 const AdditionalFeatures = props => {
+
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.store.length ? (
         <ol type="1">
           {props.store.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
+            <AdditionalFeature key={item.id} feature={item} addNewFeature={props.addNewFeature} />
           ))}
         </ol>
       ) : (
@@ -18,4 +23,11 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    store: state.store
+  }
+}
+
+export default connect(
+  mapStateToProps, { addNewFeature })(AdditionalFeatures);
